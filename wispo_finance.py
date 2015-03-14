8b1268b1767893b95b441dbe1d615fa3d67a33f4
Lucas van Dijk <info@return1.net>
2015-03-14 14:03:50 +0100

    Able to generate HTML status report using Jinja2

--- wispo_finance.py
+++ wispo_finance.py
@@ -1,7 +1,10 @@
+import os
 import sys
 import json
 from decimal import Decimal, getcontext
 import collections
+
+from jinja2 import Environment, FileSystemLoader
 
 DEFAULT_PERSONS = [
     'Tjalling',
@@ -11,40 +14,80 @@ DEFAULT_PERSONS = [
     'Lucas'
 ]
 
-def get_status(data):
-    status = collections.defaultdict(Decimal)
+ReportEntry = collections.namedtuple('ReportEntry', ['desc', 'amount'])
 
-    for payment in data:
-        status[payment['payer']] += Decimal(payment['amount'])
+class CostsSharer:
+    def __init__(self, data, default_persons=[]):
+        self.payments = data
+        self.default_persons = default_persons
 
-        persons = payment['payed_for']
+        self.status = collections.defaultdict(Decimal)
+        self.report = collections.defaultdict(list)
 
+    def get_persons(self, persons):
         if "*" in persons:
             persons.remove("*")
-            persons += DEFAULT_PERSONS
+            persons += self.default_persons
 
-        amount_per_person = Decimal(payment['amount']) / len(persons)
+        return persons
 
-        for person in persons:
-            status[person] -= amount_per_person
+    def gen_report_data(self):
+        for payment in self.payments:
+            self.status[payment['payer']] += Decimal(payment['amount'])
 
-    return status
+            persons = self.get_persons(payment['payed_for'])
+            amount_per_person = Decimal(payment['amount']) / Decimal(len(persons))
 
-def list_statusses(data):
-    for name, status in data.items():
-        print("{name}: {status}".format(name=name, status=status))
+            if payment['payer'] in persons:
+                report_amount = Decimal(payment['amount']) - amount_per_person
+            else:
+                report_amount = Decimal(payment['amount'])
 
-    print("Total: {sum}".format(sum=sum(data.values())))
+            self.report[payment['payer']].append(
+                ReportEntry(
+                    desc=payment['description'],
+                    amount=report_amount.quantize(Decimal('.01'))
+                )
+            )
+
+            for person in persons:
+                self.status[person] -= amount_per_person
+
+                if person != payment['payer']:
+                    self.report[person].append(
+                        ReportEntry(
+                            desc=payment['description'],
+                            amount=(-amount_per_person).quantize(Decimal('.01'))
+                        )
+                    )
+
+    def gen_report(self, outfile):
+        env = Environment(loader=FileSystemLoader(
+            os.path.join(os.path.dirname(__file__), 'templates')))
+        report_tpl = env.get_template('report.html')
+
+        outfile.write(
+            report_tpl.render(
+                report_data=self.report,
+                statusses=self.status,
+                total=sum(self.status.values())
+            )
+        )
 
 if __name__ == '__main__':
     getcontext().prec = 5
     filename = sys.argv[1] if len(sys.argv) > 2 else "payments.json"
+    outfilename = "report.html"
 
     with open(filename) as f:
         data = json.load(f)
 
-        statusses = get_status(data)
-        list_statusses(statusses)
+        sharer = CostsSharer(data, DEFAULT_PERSONS)
+
+        with open(outfilename, 'w') as outfile:
+            sharer.gen_report_data()
+            sharer.gen_report(outfile)
+
 
 
 
